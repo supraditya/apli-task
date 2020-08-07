0a2ff8efc453d801783588957fc7bb927c9976fb
[supraditya] <[aditya300100@gmail.com]>
2020-08-07 23:06:39 +0530

    Youtube videos added as a playlist, grades and comments are now updated in the results modal

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ let objectArray = [
   {
     number: "Question 1",
     link: "https://www.youtube.com/embed?v=CMyQqQbjknM&feature=youtu.be",
-    comment: "Comment 1",
+    comment: "",
     grade: "0",
   },
   {
     number: "Question 2",
     link: "https://www.youtube.com/embed?v=i1D0uHRxiIY&t=598s",
-    comment: "Comment 2",
+    comment: "",
     grade: "0",
   },
   {
     number: "Question 3",
     link: "https://www.youtube.com/embed?v=h2kBtivgV_I",
-    comment: "Comment 3",
+    comment: "",
     grade: "0",
   },
   {
     number: "Question 4",
-    link: "https://www.youtube.com/embed?v=hGpT3PN00oc",
-    comment: "Comment 4",
+    link: "https://www.youtube.com/watch?v=7CfzH-_Zbdc",
+    comment: "",
     grade: "0",
   },
   {
     number: "Question 5",
     link: "https://www.youtube.com/embed?v=-oOlAGKBXnA",
-    comment: "Comment 5",
+    comment: "",
     grade: "0",
   },
 ];
@@ -44,15 +44,29 @@ function loadIframe(url) {
       "></iframe>"
   );
 }
+
 function gradeClick(num, qnumber) {
   document.getElementById("score").innerText = num;
   objectArray[this.qnumber].grade = num;
+
+  // Changes grades in result modal
+  let grselector = "gr" + (this.qnumber + 1);
+  document.getElementById(grselector).textContent =
+    objectArray[this.qnumber].grade + "/5";
 }
+
 function commentSave() {
   var comment = document.getElementById("comm").value;
   objectArray[this.qnumber].comment = comment;
+
+  // Changes comments in results modal
+  let coselector = "co" + (this.qnumber + 1);
+  document.getElementById(coselector).textContent =
+    objectArray[this.qnumber].comment;
 }
+
 $(document).ready(function () {
+  // Question 1
   $("#one").click(function () {
     qnumber = 0;
     $("#videobox1").css("display", "grid");
@@ -60,34 +74,36 @@ $(document).ready(function () {
     $("#comm").val(objectArray[0].comment);
     $("#score").text(objectArray[0].grade);
   });
+  // Question 2
   $("#two").click(function () {
     loadIframe(objectArray[1].link);
     qnumber = 1;
-    $("#videobox").attr("src", objectArray[1].link);
+
     $("#qno").text(objectArray[1].number);
     $("#comm").val(objectArray[1].comment);
     $("#score").text(objectArray[1].grade);
   });
+  // Question 3
   $("#three").click(function () {
     loadIframe(objectArray[2].link);
     qnumber = 2;
-    $("#videobox").attr("src", objectArray[2].link);
+
     $("#qno").text(objectArray[2].number);
     $("#comm").val(objectArray[2].comment);
     $("#score").text(objectArray[2].grade);
   });
+  // Question 4
   $("#four").click(function () {
     loadIframe(objectArray[3].link);
     qnumber = 3;
-    $("#videobox").attr("src", objectArray[3].link);
     $("#qno").text(objectArray[3].number);
     $("#comm").val(objectArray[3].comment);
     $("#score").text(objectArray[3].grade);
   });
+  // Question 5
   $("#five").click(function () {
     qnumber = 4;
     loadIframe(objectArray[4].link);
-    $("#videobox").attr("src", objectArray[4].link);
     $("#qno").text(objectArray[4].number);
     $("#comm").val(objectArray[4].comment);
     $("#score").text(objectArray[4].grade);
